f0ed7852867ad79d392662b939769b5cd07765c9
Luis Salas <lsalas@spensiones.cl>
2024-12-06 17:28:02 -0300

    test desde endpoints

---
+++ __tests__/server.spec.ts
@@ -0,0 +1,59 @@
+import app from '../src/server.js';
+import request from 'supertest';
+
+describe("Bateria de test de servidor", () => {
+
+    test("Servidor en endpoint /", async () => {
+
+        return await request(app)
+            .get('/')
+            .expect(200)
+            .expect("Content-Type", /text/)
+            .then(response => {
+                expect(response.text).toBe("Hola mundo al usuario lsalas");
+            });
+    });
+
+    test("Servidor en endpoint /api-key", async () => {
+
+        return await request(app)
+            .get('/api-key')
+            .expect(200)
+            .expect("Content-Type", /text/)
+            .then(response => {
+                expect(response.text).toBe("la apikey de mi aplicacion es: default-key");
+            });
+    });
+
+    test("Servidor en endpoint /validar-rut?rut=numero-rut", async () => {
+
+        return await request(app)
+            .get('/validar-rut?rut=553500-K')
+            .expect(200)
+            .expect("Content-Type", /text/)
+            .then(response => {
+                expect(response.text).toBe("El rut suministrado (553500-K) es : valido");
+            });
+
+        return await request(app)
+            .get('/validar-rut?rut=1')
+            .expect(200)
+            .expect("Content-Type", /text/)
+            .then(response => {
+                expect(response.text).toBe("El rut suministrado (1) es : invalallido");
+            });
+    });
+
+    test("Servidor en endpoint /buscar-subcadena?cadena=string&subcadena=substring", async () => {
+
+        return await request(app)
+            .get('/buscar-subcadena?cadena=hola mundo y mas mundo&subcadena=mundo')
+            .expect(200)
+            .expect("Content-Type", /text/)
+            .then(response => {
+                expect(response.text).toBe("La cadeja \"hola mundo y mas mundo\" tiene 2 repeticiones de la subcadena \"mundo\"");
+            });
+    });
+
+
+});
